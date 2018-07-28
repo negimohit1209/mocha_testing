abc7df6a3ca8b68fae3a72626dd902c29e5e866d
negimohit1209 <mohitsngh1997@gmail.com>
2018-07-28 17:20:30 +0530

    add assertion library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,14 +1,37 @@
-const utils = require('./utils')
+const expect = require('expect');
+const utils = require('./utils');
+
 it('should add two numbers', () => {
     var res = utils.add(33,11);
-    if(res !== 44){
-        throw new Error(`expected 44, but got ${res}`);
-    }
+    expect(res).toBe(44).toBeA('number')
 });
 
 it('should square a number', () => {
     var res = utils.square(5);
-    if(res !== 25){
-       throw new Error(`expected 25, but got ${res}`);
-    }
+    expect(res).toBe(25).toBeA('number')
 });
+
+it('should expect some values', () => {
+    // expect(12).toNotBe(11);
+    // expect({name: 'Mohit'}).toEqual({name: 'Mohit'})
+    // expect([2,3,4]).toInclude(5);
+    expect({
+        name: 'Mohit Negi',
+        age: 21,
+        location: 'kolkata'
+    }).toInclude({
+        age: 21
+    })
+});
+
+it("should set firstname and last name" , ()=>{
+    var user = {
+        location: 'kolkata',
+        age: '21'
+    }
+    var res = utils.setName(user, 'Mohit Negi')
+    expect(res).toInclude({
+        firstName: 'Mohit',
+        lastName: 'Negi'
+    })
+})
