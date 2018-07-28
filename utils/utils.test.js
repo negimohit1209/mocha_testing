0e0a4f7a94fe8a6fd2704594cede65b3557411d8
negimohit1209 <mohitsngh1997@gmail.com>
2018-07-28 18:29:40 +0530

    add describe

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,35 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(33,11);
-    expect(res).toBe(44).toBeA('number')
-});
+describe('Utils', ()=> {
+    describe('#add', ()=> {
+        it('should add two numbers', () => {
+            var res = utils.add(33,11);
+            expect(res).toBe(44).toBeA('number')
+        });
+        it("shouls async add two numbers", (done)=>{
+            utils.asyncAdd(4, 3,(sum)=>{
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            })
+        })
+    })
+    describe('#square', ()=> {
+        it('should square a number', () => {
+            var res = utils.square(5);
+            expect(res).toBe(25).toBeA('number')
+        }); 
+        
+        
+        it("should async square a number", (done) => {
+            utils.asyncSquare(5,(res) => {
+                expect(res).toBe(25).toBeA('number');
+                done();
+            })
+        }); 
+    });
+})
 
-it('should square a number', () => {
-    var res = utils.square(5);
-    expect(res).toBe(25).toBeA('number')
-});
 
 it('should expect some values', () => {
     // expect(12).toNotBe(11);
@@ -33,19 +53,5 @@ it("should set firstname and last name" , ()=>{
     expect(res).toInclude({
         firstName: 'Mohit',
         lastName: 'Negi'
-    })
-})
-
-it("shouls async add two numbers", (done)=>{
-    utils.asyncAdd(4, 3,(sum)=>{
-        expect(sum).toBe(7).toBeA('number');
-        done();
-    })
-})
-
-it("should async square a number", (done) => {
-    utils.asyncSquare(5,(res) => {
-        expect(res).toBe(25).toBeA('number');
-        done();
     })
 })
