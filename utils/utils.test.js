e035e979f033a23f2e70333a89a2a9e6b996ed56
negimohit1209 <mohitsngh1997@gmail.com>
2018-07-28 14:54:34 +0530

    mocha test

---
+++ utils/utils.test.js
@@ -0,0 +1,14 @@
+const utils = require('./utils')
+it('should add two numbers', () => {
+    var res = utils.add(33,11);
+    if(res !== 44){
+        throw new Error(`expected 44, but got ${res}`);
+    }
+});
+
+it('should square a number', () => {
+    var res = utils.square(5);
+    if(res !== 25){
+       throw new Error(`expected 25, but got ${res}`);
+    }
+});
